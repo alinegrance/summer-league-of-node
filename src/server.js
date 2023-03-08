03e0e94d43e514960ca69d717cfec0f66e62e99a
Aline Grance <grance.aline@gmail.com>
2023-03-08 09:50:36 -0300

    Change port to env

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
 const app = require('./index');
 
-app.listen('3000', () => console.log('Server running on port 3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
